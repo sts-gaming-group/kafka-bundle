59db04d30ddb0cb8f092b9491ca504beac00de7b
Mariusz Jeruzal <mariusz.jeruzal@sts.pl>
2021-03-22 12:54:01 +0100

    refactor(SA-4490): doc blocki dla consumer interface (kafka-bundle)

--- src/Client/Contract/ConsumerInterface.php
+++ src/Client/Contract/ConsumerInterface.php
@@ -10,7 +10,18 @@ use Sts\KafkaBundle\RdKafka\Context;
 
 interface ConsumerInterface extends ClientInterface
 {
+    /**
+     * @param Message $message
+     * @param Context $context
+     * @return mixed
+     */
     public function consume(Message $message, Context $context);
+
+    /**
+     * @param KafkaException $exception
+     * @param Context $context
+     * @return mixed
+     */
     public function handleException(KafkaException $exception, Context $context);
     public function getName(): string;
 }
