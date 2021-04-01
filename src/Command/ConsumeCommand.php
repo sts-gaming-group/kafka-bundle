5c38b44c807c3117dcc51510ede891e343184cd4
Mariusz Jeruzal <mariusz.jeruzal@sts.pl>
2021-04-01 10:04:18 +0200

    feat(kafka-bundle): mozliwosc przekazywania opcji przez input (SA-4663)

--- src/Command/ConsumeCommand.php
+++ src/Command/ConsumeCommand.php
@@ -8,31 +8,37 @@ use Sts\KafkaBundle\Client\Consumer\ConsumerClient;
 use Sts\KafkaBundle\Client\Consumer\ConsumerProvider;
 use Sts\KafkaBundle\Client\Contract\ConsumerInterface;
 use Sts\KafkaBundle\Command\Traits\DescribeTrait;
+use Sts\KafkaBundle\Configuration\ConfigurationResolver;
 use Sts\KafkaBundle\Configuration\RawConfiguration;
 use Sts\KafkaBundle\Traits\AddConfigurationsToCommandTrait;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class ConsumeCommand extends Command
 {
     use AddConfigurationsToCommandTrait;
+    use DescribeTrait;
 
     protected static $defaultName = 'kafka:consumers:consume';
 
     private RawConfiguration $rawConfiguration;
     private ConsumerProvider $consumerProvider;
     private ConsumerClient $consumerClient;
+    private ConfigurationResolver $configurationResolver;
 
     public function __construct(
         RawConfiguration $rawConfiguration,
         ConsumerProvider $consumerProvider,
-        ConsumerClient $consumerClient
+        ConsumerClient $consumerClient,
+        ConfigurationResolver $configurationResolver
     ) {
         $this->rawConfiguration = $rawConfiguration;
         $this->consumerProvider = $consumerProvider;
         $this->consumerClient = $consumerClient;
+        $this->configurationResolver = $configurationResolver;
 
         parent::__construct();
     }
@@ -45,7 +51,8 @@ class ConsumeCommand extends Command
                 ConsumerInterface::class
             )
         )
-            ->addArgument('name', InputArgument::REQUIRED, 'Name of the registered consumer.');
+            ->addArgument('name', InputArgument::REQUIRED, 'Name of the registered consumer.')
+            ->addOption('describe', null, InputOption::VALUE_NONE, 'Describes consumer');
 
         $this->addConfigurations($this->rawConfiguration);
     }
@@ -53,6 +60,12 @@ class ConsumeCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $consumer = $this->consumerProvider->provide($input->getArgument('name'));
+
+        if ($input->getOption('describe')) {
+            $this->describe($this->configurationResolver->resolve($consumer), $output, $consumer);
+
+            return 0;
+        }
 
         $this->consumerClient->consume($consumer);
 
--- src/Exception/RecoverableMessageException.php
+++ src/Exception/RecoverableMessageException.php
@@ -4,21 +4,6 @@ declare(strict_types=1);
 
 namespace Sts\KafkaBundle\Exception;
 
-use Throwable;
-
 class RecoverableMessageException extends \RuntimeException
 {
-    private array $data;
-
-    public function __construct(Throwable $throwable, array $data = [])
-    {
-        $this->data = $data;
-        
-        parent::__construct($throwable->getMessage(), $throwable->getCode(), $throwable->getPrevious());
-    }
-
-    public function getData(): array
-    {
-        return $this->data;
-    }
 }
