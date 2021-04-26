4c7e55d1d971128505a4ad179970ca2c6c4bf00f
Mariusz Jeruzal <mariusz.jeruzal@sts.pl>
2021-04-26 13:04:15 +0200

    refactor(kafka-bundle): zmiana nazwy eventow (SA-4773)

--- src/Event/PostMessageConsumedEvent.php
+++ src/Event/PostMessageConsumedEvent.php
@@ -6,7 +6,7 @@ namespace Sts\KafkaBundle\Event;
 
 class PostMessageConsumedEvent extends AbstractMessageConsumedEvent
 {
-    public const EVENT_PREFIX = 'sts_kafka.post_message_consumed_event_';
+    public const EVENT_PREFIX = 'sts_kafka.post_message_consumed_';
 
     public static function getEventName(string $consumerName): string
     {
--- src/Event/PreMessageConsumedEvent.php
+++ src/Event/PreMessageConsumedEvent.php
@@ -6,7 +6,7 @@ namespace Sts\KafkaBundle\Event;
 
 class PreMessageConsumedEvent extends AbstractMessageConsumedEvent
 {
-    public const EVENT_PREFIX = 'sts_kafka.pre_message_consumed_event_';
+    public const EVENT_PREFIX = 'sts_kafka.pre_message_consumed_';
 
     public static function getEventName(string $consumerName): string
     {
